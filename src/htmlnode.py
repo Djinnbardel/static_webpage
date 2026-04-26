0ab209ef843481f6c3aa410f3cf47a3b0d3d7555
PatricD96 <patricid96@gmail.com>
2026-04-25 20:29:59 -0400

    Added LeafNode and Tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,22 @@ class HTMLNode():
 
     def __repr__(self):
         return (f"HTMLNode({self.tag},{self.value},{self.children},{self.props_to_html()})")
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        print(super().props_to_html())
+        if self.value is None:
+            raise ValueError("Error: LeafNode Must Have a Value.")
+        elif self.tag is None:
+            return self.value
+        else:
+            return (f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>")
+
+    def __repr__(self):
+        return (f"LeafNode({self.tag},{self.value},{self.props_to_html()})")
+
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,23 +1,45 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 
 class TestHTMLNode(unittest.TestCase):
-    node = HTMLNode("Has Tag","This Node has no children or props")
-    node2 = HTMLNode("Has Tag","This Node has no children",None,{"href": "https://nottasite.com", "target": "_not_applicable", "third": "_a_Test"})
+
+    ###HTMLNode Tests
     
     def test_nodeprint(self):
+        node = HTMLNode("Has Tag","This Node has no children or props")
         print("Testing HTMLNodes.")
-        print(self.node)
-        self.assertTrue(self.node, "HTMLNode(Has Tag,This Node has no children or props,None,)")
+        print(node)
+        self.assertEqual(repr(node), "HTMLNode(Has Tag,This Node has no children or props,None,)")
 
     def test_nodeURL(self):
+        node2 = HTMLNode("Has Tag","This Node has no children",None,{"href": "https://nottasite.com", "target": "_not_applicable", "third": "_a_Test"})
         print("Testing HTMLNodes with Props.")
-        print(self.node2)
-        self.assertTrue(self.node2, 'HTMLNode(Has Tag,This Node has no children,None, href="https://nottasite.com" target="_not_applicable" third="_a_Test")')    
+        print(node2)
+        self.assertEqual(repr(node2), 'HTMLNode(Has Tag,This Node has no children,None, href="https://nottasite.com" target="_not_applicable" third="_a_Test")')    
 
 
+class TestLeafNode(unittest.TestCase):
+    
+    ###LeafNode Tests
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        print("Testing LeafNode [No Props]")
+        print(node)
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "What is this Monster!", {"href": "www.monsterwow.net"})
+        print("Testing LeafNode [With Props]")
+        print(node)
+        self.assertEqual(node.to_html(), '<a href="www.monsterwow.net">What is this Monster!</a>')
+    
+    def test_leaf_to_html_x(self):
+        node = LeafNode(None, "Oh My Lord its Just Text.")
+        print("Testing LeafNode [No Tag]")
+        print(node)
+        self.assertEqual(node.to_html(), "Oh My Lord its Just Text.")
 
 if __name__ == "__main__":
     unittest.main()
